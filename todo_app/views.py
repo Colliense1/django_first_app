5f946ddacf1f1a550bea49ee03f142e6d20eeadd
Colliense Podder Prodip <colliensepodder25@gmail.com>
2024-08-31 23:55:31 +0600

    Django Form

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from django.http import HttpResponse
+from .forms import TaskForm
 
 # Create your views here.
 def task_list(request):
@@ -15,3 +17,39 @@ def task_list(request):
 def task_details(request, pk):
     task = Task.objects.get(pk=pk)
     return render(request, "task_detail.html", {"tasks":task})
+# Manually Add Task
+def add_task(request):
+    _title = "Let's have dinner together x"
+    _description = "Dinner Invitation at chef's table x"
+    _completed =  False
+    _due_date = "2024-08-28"
+    task = Task(title = _title , description = _description , completed = _completed , due_date = _due_date)
+    task.save()
+    #return HttpResponse("Adding Task")
+    return redirect('task_list')
+
+# CRUD (Create, Read, Update, Delete)
+
+def delete_task(request, pk):
+    try:
+        task = Task.objects.get(pk=pk)
+        task.delete()
+        return redirect('task_list')
+    except Task.DoesNotExist:
+        return HttpResponse("Task Does Not Exist")
+    
+def update_task(request):
+    task = Task.objects.get(pk=5)
+    task.title = "This is the modified title"
+    task.save()
+    return redirect('task_list')
+
+def add_task_form(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if (form.is_valid()):
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+        return render(request, "add_task.html", {"formx": form})
